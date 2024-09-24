943b770fe5a497a8b9a5a4f8d13d38af07d81d1d
hussein430 <hussen.alaa1222@gmail.com>
2024-09-24 20:38:15 +0300

    everthing work perfect before add the description for the courses

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let courses = document.querySelectorAll('.carousel-cards .carousel-item')
+
+courses.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      next = courses[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
