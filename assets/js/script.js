d34533cdff87a30380ae417aade3610f0653e2fc
hussein430 <hussen.alaa1222@gmail.com>
2024-09-26 08:39:29 +0300

    Finish the courses section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-let courses = document.querySelectorAll('.carousel-cards .carousel-item')
+let tabs = Array.from(document.querySelectorAll("button[data-tab]"));
 
-courses.forEach((el) => {
-  const minPerSlide = 4;
-  let next = el.nextElementSibling;
-  for (var i = 1; i < minPerSlide; i++) {
-    if (!next) {
-      next = courses[0];
-    }
-    let cloneChild = next.cloneNode(true);
-    el.appendChild(cloneChild.children[0]);
-    next = next.nextElementSibling;
+tabs.map(tab => {
+  if (tabs.length > 0) {
+    tabs[0].click();
   }
+  tab.addEventListener("click", function() {
+    let selector = `.featured-courses-${tab.dataset.tab} .carousel-item`;
+    
+    let courses = document.querySelectorAll(selector);
+
+    courses.forEach((el) => {
+      const minPerSlide = 4;
+      let next = el.nextElementSibling;
+      for (let i = 1; i < minPerSlide; i++) {
+        if (!next) {
+          next = courses[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+      }
+    });
+  });
 });
